4c68cbcdaf996e355d865730243ec77208bff8b8
Михаил Ярков <Thorns203519@yandex.ru>
2026-04-16 21:17:50 +0300

    new

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,13 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path(
-    'posts/<int:id>/', 
-    views.post_detail, 
-    name='post_detail'
-),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+        'posts/<int:id>/',
+        views.post_detail,
+        name='post_detail'
+    ),
+    path(
+        'category/<slug:category_slug>/',
+        views.category_posts,
+        name='category_posts'
+    ),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 def about(request):
     return render(request, 'about.html')
+
+
 def rules(request):
     return render(request, 'rules.html')
